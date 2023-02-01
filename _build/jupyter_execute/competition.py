6772bc6bd4072c0df444a05339b54a41b99ed1af
naftalic <naftalic@gmail.com>
2023-01-31 21:47:41 -0500

    adding notes

---
+++ _build/jupyter_execute/competition.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Investment Competition
+# 
+# 
+# # Some Code
+
+# In[1]:
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+w = np.linspace(0,1,100)
+sr = (2*w+(1-w))/np.sqrt(20*w**2+10*(1-w)**2)
+
+print( w[np.argmax(sr)], np.max(sr) )
+
+
+# In[2]:
+
+
+plt.plot(w,sr)
+plt.show()
+
