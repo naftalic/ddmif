088d578ebfc909a4a492e0c5a546c48efda8fd20
naftalic <naftalic@gmail.com>
2023-02-13 08:58:25 -0500

    adding notes

--- _build/jupyter_execute/causal_inference.py
+++ _build/jupyter_execute/causal_inference.py
@@ -1,7 +1,26 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# One result that will be very useful throughout this section is:
+# # Causal Inference in Finance
+# 
+# # Math background
+# The expected value, also known as the population mean, of a random variable is calculated as the weighted average of all possible values that the variable can take, where the weights are given by the probabilities of each value's occurrence in the population.
+# \begin{align}
+#   E(X) & = x_1p(x_1)+x_2p(x_2)+\dots+x_kp(x_k) \\
+#   & = \sum_{j=1}^k x_jp(x_j)              
+# \end{align}
+# 
+# The variance of a random variable in the population is defined as
+# \begin{align}
+#    V(X)=\sigma^2=E\Big[\big(X-E(X)\big)^2\Big]\ =E(X^2) - E(X)^2
+# \end{align}
+# The last identity follows from the fact that 
+# \begin{align}
+# E\Big[\big(X-E(X)\big)^2\Big]\  =
+# & = E\Big[\big(X^2-2XE(X)+E^2(X)\big)\Big]\
+# & = E(X^2)-2E(X)E(X)+E^2(X)\
+# & = E(X^2)-E^2(X)\
+# \end{align}
 # 
 # \begin{align}
 #   \sum_{i=1}^n(x_i-\overline{x})^2 & =                                                                                               
@@ -16,7 +35,6 @@
 # \end{align}
 # 
 # A more general version of this result is:
-# 
 # \begin{align}
 #    \sum_{i=1}^n(x_i-\overline{x})(y_i-\overline{y}) & = \sum_{i=1}^n x_i(y_i - \overline{y}) \\
 #     & = \sum_{i=1}^n (x_i - \overline{x})y_i \\
