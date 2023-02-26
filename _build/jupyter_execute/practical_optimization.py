2a1a354758567a445117e5f3bba89b8f46b5ee60
naftalic <naftalic@gmail.com>
2023-02-25 23:07:22 -0500

    adding notes

--- _build/jupyter_execute/practical_optimization.py
+++ _build/jupyter_execute/practical_optimization.py
@@ -98,9 +98,9 @@ print("Optimal solution:", x.level())
 # # Binary optimization problem 
 # $$
 # \begin{aligned}
-# &\text{minimize } &&x_1 + x_2 + x_3 \
-# &\text{subject to } &&x_1 + x_2 \ge 2 \
-# &&&x_2 + x_3 \le 1 \
+# &\text{minimize } &&x_1 + x_2 + x_3 \\
+# &\text{subject to } &&x_1 + x_2 \ge 2 \\
+# &&&x_2 + x_3 \le 1 \\
 # &&&x_1, x_2, x_3 \text{ are binary variables}
 # \end{aligned}
 # $$
